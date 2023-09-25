01d4d451f0cef6b4213b7a4c5ffb85c5457becce
chandubhati1997 <104966485+chandubhati1997@users.noreply.github.com>
2023-09-25 12:28:36 +0000

    post function added

--- .devcontainer/todo_app/index.js
+++ .devcontainer/todo_app/index.js
@@ -17,6 +17,30 @@ app.get('/todo', (req, res) => {
   })
 })
 
+app.post('/todo',(req,res) => {
+
+    const newtodo = {
+        id: Math.floor(Math.random()*1000000),  //unique number every time id is generated
+        title: req.body.title,
+        description: req.body.description
+
+    } 
+
+    fs.readFile("todo.json","utf-8",(err,data) =>{
+        if(err) throw err;
+        const todos =JSON.parse(data)
+        todos.push(newtodo);
+
+        fs.writeFile("todo.json",JSON.stringify(todos),(err) => {
+            if(err) throw err;
+            res.status(201).json(newtodo);
+        })
+
+    })
+
+
+})
+
 
 
 app.listen(port, () => {
