4e0ef399b3825025f3d25b16bee6b325c7e357df
chandubhati1997 <104966485+chandubhati1997@users.noreply.github.com>
2023-09-25 12:08:35 +0000

    changes get involved

--- .devcontainer/todo_app/index.js
+++ .devcontainer/todo_app/index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('fs')
+const bodyParser = require("body-parser")
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(bodyParser.json());
+
+
+
+
+app.get('/todo', (req, res) => {
+
+  fs.readFile("todo.json","utf-8",(err,data) =>  {
+    if(err) throw err;
+    res.json(JSON.parse(data));
+  })
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
